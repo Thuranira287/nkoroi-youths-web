e3603c2f50272509106e04e8319b2c3b97065761
Alexander Thuranira <alexanderthuranira287@gmail.com>
2025-08-26 09:53:18 +0300

    database query

--- server/database/pg.ts
+++ server/database/pg.ts
@@ -3,7 +3,7 @@ import { Pool } from "pg";
 
 // Netlify (with Neon) will provide DATABASE_URL automatically
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.NETLIFY_DATABASE_URL,
   ssl: {
     rejectUnauthorized: false, // important for cloud DBs like Neon
   },
